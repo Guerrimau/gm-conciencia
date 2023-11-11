b3d2ecf839c7028a589481fe87764dee1def4fb4
Gerardo Mayboca <gerardo@risingcloud.com>
2023-11-11 12:41:53 -0700

    Updated styles of contact section

--- src/components/contact-section/index.jsx
+++ src/components/contact-section/index.jsx
@@ -2,11 +2,10 @@ import { Button, Input } from '@nextui-org/react'
 
 const ContactSection = () => {
   return (
-    <section id="contact" className="rounded-3xl bg-white mt-36 p-10 flex justify-between">
-      <div className="w-1/2 space-y-5">
+    <section id="contact" className="rounded-3xl bg-white mt-36 p-10 flex justify-between items-center">
+      <div className="w-1/2 space-y-8">
         <h3 className="text-5xl">¡Inicia tu viaje hacia el bienestar ahora!</h3>
         <p className="text-xl">Nuestro equipo de psiquiatras altamente capacitados está listo para proporcionarte el apoyo que necesitas. Completa el formulario a continuación para agendar tu consulta. Nos pondremos en contacto contigo en breve.</p>
-        
       </div>
       <div className="w-[450px] space-y-5 flex flex-col">
         <p className="text-zinc-500">Ingresa tus datos</p>
