f4291400d30ec415686473d78466131d9fb08a11
Gerardo Mayboca <gerardo@risingcloud.com>
2023-11-11 12:41:07 -0700

    Updated order of contact section form

--- src/components/contact-section/index.jsx
+++ src/components/contact-section/index.jsx
@@ -4,15 +4,16 @@ const ContactSection = () => {
   return (
     <section id="contact" className="rounded-3xl bg-white mt-36 p-10 flex justify-between">
       <div className="w-1/2 space-y-5">
-        <h3 className="text-5xl">¡Inicia tu Viaje hacia el Bienestar Ahora!</h3>
+        <h3 className="text-5xl">¡Inicia tu viaje hacia el bienestar ahora!</h3>
         <p className="text-xl">Nuestro equipo de psiquiatras altamente capacitados está listo para proporcionarte el apoyo que necesitas. Completa el formulario a continuación para agendar tu consulta. Nos pondremos en contacto contigo en breve.</p>
-        <Button size="lg" variant="solid" color="primary">Enviar</Button>
+        
       </div>
-      <div className="w-[450px] space-y-5">
+      <div className="w-[450px] space-y-5 flex flex-col">
         <p className="text-zinc-500">Ingresa tus datos</p>
         <Input color="primary" size="lg" type="text" variant="flat" label="Nombre" />
         <Input color="primary" size="lg" type="email" variant="flat" label="Correo" />
         <Input color="primary" size="lg" type="tel" variant="flat"  label="Celular"/>
+        <Button className="w-fit self-end" size="lg" variant="solid" color="primary">Enviar</Button>
       </div>
     </section>
   )
