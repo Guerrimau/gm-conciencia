f16f958f683302d199391e5893d26ab6cfd5bdc9
Gerardo Mayboca <gmayboca@gmail.com>
2023-11-24 16:16:24 -0700

    Something

--- src/components/landing-section/index.jsx
+++ src/components/landing-section/index.jsx
@@ -6,15 +6,27 @@ const LandingSection = () => {
   return (
     <section id="landing" className="flex items-center p-10 gap-5">
       <div className="space-y-8">
-        <h1 className="text-4xl md:text-6xl font-bold">¿Cómo podemos ayudarte hoy?</h1>
-        <h2 className="text-xl text-zinc-500">Nuestro equipo de psiquiatras altamente calificados en Apodaca está aquí para acompañarte en tu viaje hacia el bienestar emocional.</h2>
-        <a href="https://wa.link/vlynwc" target="_blank">
-          <Button className="w-full md:w-fit" size="lg" color="primary">Contáctanos</Button>
-        </a>
+        <h1 className="text-4xl md:text-6xl font-bold">
+          ¿Cómo podemos ayudarte hoy?
+        </h1>
+        <h2 className="text-xl text-zinc-500">
+          Nuestro equipo de psiquiatras altamente calificados en Apodaca está
+          aquí para acompañarte en tu viaje hacia el bienestar emocional.
+        </h2>
+        <Button className="w-full md:w-fit" size="lg" color="primary">
+          <a className="mt-5" href="https://wa.link/vlynwc" target="_blank">
+            Contáctanos
+          </a>
+        </Button>
       </div>
-      <Image className="hidden rounded-full md:block" width={500} alt="consciencia hero image" src={heroImage} />
+      <Image
+        className="hidden rounded-full md:block"
+        width={500}
+        alt="consciencia hero image"
+        src={heroImage}
+      />
     </section>
-  )
-}
+  );
+};
 
 export default LandingSection;
