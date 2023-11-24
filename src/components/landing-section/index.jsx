22cfa7a6466252ebc8fd48509499b666faaa4b77
Gerardo Mayboca <gmayboca@gmail.com>
2023-11-24 16:13:07 -0700

    Added whatsapp link

--- src/components/landing-section/index.jsx
+++ src/components/landing-section/index.jsx
@@ -8,7 +8,9 @@ const LandingSection = () => {
       <div className="space-y-8">
         <h1 className="text-4xl md:text-6xl font-bold">¿Cómo podemos ayudarte hoy?</h1>
         <h2 className="text-xl text-zinc-500">Nuestro equipo de psiquiatras altamente calificados en Apodaca está aquí para acompañarte en tu viaje hacia el bienestar emocional.</h2>
-        <Button className="w-full md:w-fit" size="lg" color="primary">Contáctanos</Button>
+        <a href="https://wa.link/vlynwc" target="_blank">
+          <Button className="w-full md:w-fit" size="lg" color="primary">Contáctanos</Button>
+        </a>
       </div>
       <Image className="hidden rounded-full md:block" width={500} alt="consciencia hero image" src={heroImage} />
     </section>
